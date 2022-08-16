fa2e4a114a19977d35207257576787d5463dc73d
FelipeBurboa <felipeburboa1991@gmail.com>
2022-08-16 12:06:14 -0400

    Tables created

---
+++ db/migrate/20220816160134_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :courses do |t|
+      t.string :code
+      t.date :startdate
+      t.date :enddate
+      t.references :plan, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
